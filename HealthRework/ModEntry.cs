6d3e77a7a50ab9c7c1365f3fe4e4c01251253059
Asim Seth <131764979+seferoni@users.noreply.github.com>
2024-04-04 10:04:12 +0630

    Initial commit

---
+++ HealthRework/ModEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace HealthRework
+{	// TODO: need to implement custom event
+	internal sealed class ModEntry : Mod
+	{
+		private readonly Dictionary<string, float> Parameters = new()
+		{
+			{"HealthFloor", 25f}
+		};
+
+		public override void Entry(IModHelper helper)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
